bc77c02f265dbdf3af8ee1c46b537e1b6338cf26
FernandoBecerraOrtiz <fbecerrao01@suarezdefigueroa.es>
2023-10-15 20:01:46 +0200

    Agregado variable producto y calculo del mismo

--- Ej.java
+++ Ej.java
@@ -4,13 +4,16 @@ public class Ej {
 
 	public static void main(String[] args) {
 		
-		int a,b,suma;
+		int a,b,suma,producto;
 		
 		a=7;
 		b=6;
 		
 		suma = a+b;
 		System.out.println(suma);
+		
+		producto=suma*suma;
+		System.out.println(producto);
 	}
 
 }
