87d24221a3faf819ca2faeb00eec929e154ea6ea
FernandoBecerraOrtiz <fbecerrao01@suarezdefigueroa.es>
2023-10-15 20:19:02 +0200

    Agregado division y calculo de la division

--- Ej2.java
+++ Ej2.java
@@ -4,13 +4,17 @@ public class Ej2 {
 
 	public static void main(String[] args) {
 		
-		int a,b,resta;
+		int a,b,resta,division;
 		
 		a=10;
 		b=5;
 		
 		resta = a-b;
 		System.out.println(resta);
+		
+		
+		division=resta/resta;
+		System.out.println(division);
 	}
 
 }
