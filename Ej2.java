9336a0900c509f70359436434d52cafbeb9caa7d
FernandoBecerraOrtiz <fbecerrao01@suarezdefigueroa.es>
2023-10-15 20:17:30 +0200

    Cambiar numelos variables y declarar resta

---
+++ Ej2.java
@@ -0,0 +1,16 @@
+package pruebaGit;
+
+public class Ej2 {
+
+	public static void main(String[] args) {
+		
+		int a,b,resta;
+		
+		a=10;
+		b=5;
+		
+		resta = a-b;
+		System.out.println(resta);
+	}
+
+}
