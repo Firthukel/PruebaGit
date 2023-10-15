8a313609d5d6625a654735fb2df648fd3f67bc1c
FernandoBecerraOrtiz <fbecerrao01@suarezdefigueroa.es>
2023-10-15 19:53:25 +0200

    Declaracion de suma de proyecto java

---
+++ Ej.java
@@ -0,0 +1,16 @@
+package pruebaGit;
+
+public class Ej {
+
+	public static void main(String[] args) {
+		
+		int a,b,suma;
+		
+		a=7;
+		b=6;
+		
+		suma = a+b;
+		System.out.println(suma);
+	}
+
+}
